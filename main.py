ea2ef817941d68efad4567a09b6962f58203b879
XinchenZhong <xinchen.zhong.19@ucl.ac.uk>
2019-12-24 21:55:45 +0000

    A basic sketch of the "main.py"。

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def main():
-    print("assignment2")
-    print("test")
+    # Task1: User Input
+    print("Please input your current location: ")
+
+    # Task2: Highest Point Identification
+
+    # Task 3: Nearest Integrated Transport Network
+
+    # Task 4: Shortest Path
+
+    # Task 5: Map Plotting
+
+    # Task 6: Extend the Region
 
 
 if __name__ == "__main__":
     main()
-    i=0
-    o=0
