61b3c4ec9c6c3891b85d457e3e9e25e0c4c781ac
Xinchen Zhong <xinchen.zhong.19@ucl.ac.uk>
2019-12-24 23:55:32 +0000

    feat: asked the user to input location and added error handling functionality

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import sys
+
+
 def main():
     # Task1: User Input
     print("Please input your current location: ")
+    tuple_user = (sys.argv[1], sys.argv[2])
+    # Error handling
+    try:
+        x = float(tuple_user[0])
+        y = float(tuple_user[1])
+    except ValueError:
+        print("The coordinate should be number, please input again: ")
+        while True:
+            try:
+                x = float(input("x coordinate: "))
+                y = float(input("y coordinate: "))
+                break
+            except ValueError:
+                print("The coordinate should be number, please input again: ")
+
 
     # Task2: Highest Point Identification
 
