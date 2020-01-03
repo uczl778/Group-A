08142fcc48d682a77f735f7a722a5a0cade1f296
uczl778 <uczl778@ucl.ac.uk>
2020-01-03 00:33:41 +0000

    get the node id

--- ITN.py
+++ ITN.py
@@ -2,23 +2,28 @@ from rtree import *
 import networkx as nx
 import json
 
+solent_itn_json = "E:/pycharm/ass2/Material/itn/solent_itn.json"
+with open('solent_itn.json', "r") as load_f:
+    #print(load_dict['roadlinks'])
+    solent_itn = json.load(load_f)
+#get the road id
+road_nodes = solent_itn['roadnodes']
+for road_id in road_nodes:
+    node_coords = road_nodes[road_id]['coords']
+    print(node_coords)
+#print(load_dict[''][1])
+
+
 idx = index.Index()
-br = (430000, 80000, 465000, 95000)
+br = (1, 1, 20, 20)
 idx.insert(0, br)
 for i in range(1000):
     for j in range(1000):
         idx.insert(i * 1000 + j, (i, j, i + 10, j + 10))
-for i in idx.intersection((1.0, 1.0, 2.0, 2.0)):
-    print(i)
-for i in idx.nearest((0.8, 0.8), 1):
-    print(i)
+for i in idx.intersection((1.0, 1.0, 20.0, 20.0)):
+     print(i)
+for i in idx.nearest((20,20), 1):
+     print(i)
 
-g = nx.Graph()
-solent_itn_json = "E:/pycharm/ass2/Material/itn/solent_itn.json"
-with open(solent_itn_json, "r") as f:
-    solent_itn = json.load(f)
 
-g = nx.Graph()
-road_links = solent_itn['roadlinks']
-for link in road_links:
-    g.add_edge(road_links[link]['start'], road_links[link]['end'], fid=link, weight=road_links[link]['length'])
+
