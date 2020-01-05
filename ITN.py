3d9de20710d0ed8b481edb92b8d231a1923030a0
uczl778 <uczl778@ucl.ac.uk>
2020-01-05 23:15:04 +0000

    the first draft of task 3

--- ITN.py
+++ ITN.py
@@ -7,54 +7,54 @@ with open('solent_itn.json', "r") as load_f:
     solent_itn = json.load(load_f)
 
 #create the bounding box
-idx = index.Index()
-index = 0
-idx.insert(index,(430000,465000,80000,95000))
-index = index + 1
+
+# idx.insert(index,(430000,80000,465000,95000))
+# index = index + 1
 
 road_nodes = solent_itn['roadnodes']
-idx = list(road_nodes)
+# idx = list(road_nodes)
+# print(idx)
 #create a list of the coords
-road_nodes = []
+# noad_list = {}
+idx = index.Index()
+index = 0
+noad_append_test = []
 
 for coords in road_nodes:
-    node_coords = road_nodes[road_id]['coords']
-    road_nodes.append(coords(1,node_coords))
+    node_coords = road_nodes[coords]['coords']
+    road_id = coords
+    # noad_list[road_id] = node_coords
+    noad_append_test.append(node_coords)
+print(noad_append_test)
 
-road_links = solent_itn['roadlinks']
-for link in roadlinks:
+# road_links = solent_itn['roadlinks']
 
-# firstnode = [0,0]
+#firstnode = noad_append_test[0]
+#print(firstnode)
 # endnode = [3,3]
 
-previousnode = start
-for node in coords:
-    idx.insert(index, (firstnode[0], firstnode[1],node[0], node[1]))
-    previousnode = node
-    index = index + 1
+#previousnode = firstnode
+for node in noad_append_test:
+     idx.insert(index, (node[0], node[1], node[0], node[1]))
+     #previousnode = node
+     index = index + 1
 
-for i in range(100):
-    for j in range(100):
-        idx.insert(i*100 + j, (i, j, i+0.99, j+0.99))
 
-for i in idx.intersection((x - 5000, y - 5000, x + 5000, y +5000)):
+
+# for i in range(100):
+#     for j in range(100):
+#         idx.insert(i*100 + j, (i, j, i+0.99, j+0.99))
+#
+#print(list(idx.intersection((442798, 75320, 452798, 85320))))
+# for i in set(idx.intersection((442798, 75320, 452798, 85320))):
     # User-centered rectangle with a radius of 5 kilometers
-    print(i)
+    # print(i)
+
+x = 442798
+y = 75320
 
 for i in idx.nearest((x, y), 1):
     #（x，y） user's coordinate
     print(i)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(noad_append_test[2288])
